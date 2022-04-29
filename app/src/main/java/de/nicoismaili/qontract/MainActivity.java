5fd4a983c6614a94c299d8c9f7dac3325a187e53
Nico Ismaili <nico@ismaili.de>
2022-04-29 20:05:36 +0200

    Updated ContractsRepository Instantiation
    Signed-off-by: Nico Ismaili <nico@ismaili.de>

--- app/src/main/java/de/nicoismaili/qontract/MainActivity.java
+++ app/src/main/java/de/nicoismaili/qontract/MainActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import de.nicoismaili.qontract.data.ContractsRepository;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ContractsRepository contractsRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.contractsRepository = new ContractsRepository(this);
         setContentView(R.layout.activity_main);
     }
 }

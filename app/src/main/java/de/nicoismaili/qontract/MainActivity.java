68e29076e3a43862365a22405371c20d5fcfe5b3
Nico Ismaili <nico@ismaili.de>
2022-05-19 19:33:46 +0200

    Added doc

--- app/src/main/java/de/nicoismaili/qontract/MainActivity.java
+++ app/src/main/java/de/nicoismaili/qontract/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -12,42 +13,77 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+/**
+ * The main activity which hosts all Fragments and the AppBar.
+ */
 public class MainActivity extends AppCompatActivity {
 
-  private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.main_menu, menu);
-    return true;
-  }
+    /**
+     * Initialize the contents of the Activity's standard options menu.
+     *
+     * @param menu The options menu in which items are placed.
+     * @return true to display the menu
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    NavHostFragment hostFragment =
-        (NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-    assert hostFragment != null;
-    NavController navController = hostFragment.getNavController();
-    Toolbar toolbar = findViewById(R.id.main_app_bar);
-    setSupportActionBar(toolbar);
-    this.appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-  }
+    /**
+     * Called when the activity is starting. Performs initialization of the navigation and the appbar.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Inflate host fragment layout with navigation component
+        NavHostFragment hostFragment =
+                (NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert hostFragment != null;
+        this.navController = hostFragment.getNavController();
+        // Set toolbar as this activity's action bar and initialize it with NavigationUI
+        Toolbar toolbar = findViewById(R.id.main_app_bar);
+        setSupportActionBar(toolbar);
+        this.appBarConfiguration = new AppBarConfiguration.Builder(this.navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, this.navController, appBarConfiguration);
+    }
 
-  @SuppressLint("InflateParams")
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    return NavigationUI.onNavDestinationSelected(item, navController)
-        || super.onOptionsItemSelected(item);
-  }
+    /**
+     * This hook is called whenever an item in the
+     * options menu is selected. Overriding this
+     * method is necessary to make the AppBar dependent
+     * on the navigation.
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @SuppressLint("InflateParams")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, this.navController)
+                || super.onOptionsItemSelected(item);
+    }
 
-  @Override
-  public boolean onSupportNavigateUp() {
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    return NavigationUI.navigateUp(navController, appBarConfiguration)
-        || super.onSupportNavigateUp();
-  }
+    /**
+     * This method is called whenever the user chooses to navigate Up within your application's
+     * activity hierarchy from the action bar. Overriding this
+     * method is also necessary to make the AppBar dependent
+     * on the navigation.
+     *
+     * @return true if Up navigation completed successfully and this Activity was finished,
+     * false otherwise.
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(this.navController, this.appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
--- app/src/main/java/de/nicoismaili/qontract/data/contract/pojo/Contract.java
+++ app/src/main/java/de/nicoismaili/qontract/data/contract/pojo/Contract.java
@@ -1,13 +1,16 @@
 package de.nicoismaili.qontract.data.contract.pojo;
 
 import android.graphics.Bitmap;
+
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
+
 import de.nicoismaili.qontract.data.contract.ContractConverters;
+
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,223 +19,224 @@ import java.util.Locale;
 import java.util.Objects;
 
 /**
- * Contract entity containing Room information to create contract rows in the database.
- *
- * @author Nico Ismaili
+ * Contract POJO (and ROOM {@link Entity}) containing
+ * all necessary information to model a contract.
  */
 @Entity(tableName = "contracts")
 @TypeConverters({ContractConverters.class})
 public class Contract implements Serializable {
-  @Ignore public static final String DATE_STRING_FORMAT = "dd.MM.yyyy";
+    @Ignore
+    public static final String DATE_STRING_FORMAT = "dd.MM.yyyy";
 
-  @PrimaryKey(autoGenerate = true)
-  @ColumnInfo(name = "contract_id")
-  private int id;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "contract_id")
+    private int id;
 
-  @ColumnInfo(name = "read")
-  private boolean isRead;
+    @ColumnInfo(name = "read")
+    private boolean isRead;
 
-  private long dateLong;
-  @NonNull private String location;
+    private long dateLong;
+    @NonNull
+    private String location;
 
-  @NonNull
-  @ColumnInfo(name = "model_first_name")
-  private String modelFirstname;
+    @NonNull
+    @ColumnInfo(name = "model_first_name")
+    private String modelFirstname;
 
-  @NonNull
-  @ColumnInfo(name = "model_last_name")
-  private String modelLastname;
+    @NonNull
+    @ColumnInfo(name = "model_last_name")
+    private String modelLastname;
 
-  private String modelAddress;
-  private String modelPhone;
-  private String modelEmail;
+    private String modelAddress;
+    private String modelPhone;
+    private String modelEmail;
 
-  @ColumnInfo(name = "images_json")
-  // Contains paths to the images belonging to a contract
-  private List<String> images;
-  // Contains the signature of the model as an SVG
-  private Bitmap modelSignature;
+    @ColumnInfo(name = "images_json")
+    // Contains paths to the images belonging to a contract
+    private List<String> images;
+    // Contains the signature of the model as an SVG
+    private Bitmap modelSignature;
 
-  @Ignore
-  public Contract(Contract another) {
-    this.id = another.id;
-    this.isRead = another.isRead;
-    this.dateLong = another.dateLong;
-    this.location = another.location;
-    this.modelFirstname = another.modelFirstname;
-    this.modelLastname = another.modelLastname;
-    this.modelAddress = another.modelAddress;
-    this.modelPhone = another.modelPhone;
-    this.modelEmail = another.modelEmail;
-    this.images = another.images;
-    this.modelSignature = another.modelSignature;
-  }
+    @Ignore
+    public Contract(Contract another) {
+        this.id = another.id;
+        this.isRead = another.isRead;
+        this.dateLong = another.dateLong;
+        this.location = another.location;
+        this.modelFirstname = another.modelFirstname;
+        this.modelLastname = another.modelLastname;
+        this.modelAddress = another.modelAddress;
+        this.modelPhone = another.modelPhone;
+        this.modelEmail = another.modelEmail;
+        this.images = another.images;
+        this.modelSignature = another.modelSignature;
+    }
 
-  public Contract() {
-    location = "";
-    modelLastname = "";
-    modelFirstname = "";
-    dateLong = new Date().getTime();
-    modelSignature = null;
-  }
+    public Contract() {
+        location = "";
+        modelLastname = "";
+        modelFirstname = "";
+        dateLong = new Date().getTime();
+        modelSignature = null;
+    }
 
-  @NonNull
-  @Override
-  public String toString() {
-    SimpleDateFormat sdf = new SimpleDateFormat(DATE_STRING_FORMAT, Locale.ROOT);
-    return String.format(
-        "Model Release Form (%s, %s)\n\n- Model -\n\n%s %s\n%s%s%ssigned: %s",
-        sdf.format(new Date(dateLong)),
-        location,
-        modelFirstname,
-        modelLastname,
-        modelAddress != null && !modelAddress.isEmpty() ? modelAddress + '\n' : "",
-        modelPhone != null && !modelPhone.isEmpty() ? modelPhone + '\n' : "",
-        modelEmail != null && !modelEmail.isEmpty() ? modelEmail + '\n' : "",
-        modelSignature != null);
-  }
+    @NonNull
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_STRING_FORMAT, Locale.ROOT);
+        return String.format(
+                "Model Release Form (%s, %s)\n\n- Model -\n\n%s %s\n%s%s%ssigned: %s",
+                sdf.format(new Date(dateLong)),
+                location,
+                modelFirstname,
+                modelLastname,
+                modelAddress != null && !modelAddress.isEmpty() ? modelAddress + '\n' : "",
+                modelPhone != null && !modelPhone.isEmpty() ? modelPhone + '\n' : "",
+                modelEmail != null && !modelEmail.isEmpty() ? modelEmail + '\n' : "",
+                modelSignature != null);
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    Contract contract = (Contract) o;
+        Contract contract = (Contract) o;
 
-    if (id != contract.id) return false;
-    if (isRead != contract.isRead) return false;
-    if (dateLong != contract.dateLong) return false;
-    if (!location.equals(contract.location)) return false;
-    if (!modelFirstname.equals(contract.modelFirstname)) return false;
-    if (!modelLastname.equals(contract.modelLastname)) return false;
-    if (!Objects.equals(modelAddress, contract.modelAddress)) return false;
-    if (!Objects.equals(modelPhone, contract.modelPhone)) return false;
-    if (!Objects.equals(modelEmail, contract.modelEmail)) return false;
-    if (!Objects.equals(images, contract.images)) return false;
-    return Objects.equals(modelSignature, contract.modelSignature);
-  }
+        if (id != contract.id) return false;
+        if (isRead != contract.isRead) return false;
+        if (dateLong != contract.dateLong) return false;
+        if (!location.equals(contract.location)) return false;
+        if (!modelFirstname.equals(contract.modelFirstname)) return false;
+        if (!modelLastname.equals(contract.modelLastname)) return false;
+        if (!Objects.equals(modelAddress, contract.modelAddress)) return false;
+        if (!Objects.equals(modelPhone, contract.modelPhone)) return false;
+        if (!Objects.equals(modelEmail, contract.modelEmail)) return false;
+        if (!Objects.equals(images, contract.images)) return false;
+        return Objects.equals(modelSignature, contract.modelSignature);
+    }
 
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (isRead ? 1 : 0);
-    result = 31 * result + (int) (dateLong ^ (dateLong >>> 32));
-    result = 31 * result + location.hashCode();
-    result = 31 * result + modelFirstname.hashCode();
-    result = 31 * result + modelLastname.hashCode();
-    result = 31 * result + (modelAddress != null ? modelAddress.hashCode() : 0);
-    result = 31 * result + (modelPhone != null ? modelPhone.hashCode() : 0);
-    result = 31 * result + (modelEmail != null ? modelEmail.hashCode() : 0);
-    result = 31 * result + (images != null ? images.hashCode() : 0);
-    result = 31 * result + (modelSignature != null ? modelSignature.hashCode() : 0);
-    return result;
-  }
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (isRead ? 1 : 0);
+        result = 31 * result + (int) (dateLong ^ (dateLong >>> 32));
+        result = 31 * result + location.hashCode();
+        result = 31 * result + modelFirstname.hashCode();
+        result = 31 * result + modelLastname.hashCode();
+        result = 31 * result + (modelAddress != null ? modelAddress.hashCode() : 0);
+        result = 31 * result + (modelPhone != null ? modelPhone.hashCode() : 0);
+        result = 31 * result + (modelEmail != null ? modelEmail.hashCode() : 0);
+        result = 31 * result + (images != null ? images.hashCode() : 0);
+        result = 31 * result + (modelSignature != null ? modelSignature.hashCode() : 0);
+        return result;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public boolean isRead() {
-    return isRead;
-  }
+    public boolean isRead() {
+        return isRead;
+    }
 
-  public void setRead(boolean read) {
-    isRead = read;
-  }
+    public void setRead(boolean read) {
+        isRead = read;
+    }
 
-  public long getDateLong() {
-    return dateLong;
-  }
+    public long getDateLong() {
+        return dateLong;
+    }
 
-  public void setDateLong(long dateLong) {
-    this.dateLong = dateLong;
-  }
+    public void setDateLong(long dateLong) {
+        this.dateLong = dateLong;
+    }
 
-  @NonNull
-  public String getLocation() {
-    return location;
-  }
+    @NonNull
+    public String getLocation() {
+        return location;
+    }
 
-  public void setLocation(@NonNull String location) {
-    this.location = location;
-  }
+    public void setLocation(@NonNull String location) {
+        this.location = location;
+    }
 
-  @NonNull
-  public String getModelFirstname() {
-    return modelFirstname;
-  }
+    @NonNull
+    public String getModelFirstname() {
+        return modelFirstname;
+    }
 
-  public void setModelFirstname(@NonNull String modelFirstname) {
-    this.modelFirstname = modelFirstname;
-  }
+    public void setModelFirstname(@NonNull String modelFirstname) {
+        this.modelFirstname = modelFirstname;
+    }
 
-  @NonNull
-  public String getModelLastname() {
-    return modelLastname;
-  }
+    @NonNull
+    public String getModelLastname() {
+        return modelLastname;
+    }
 
-  public void setModelLastname(@NonNull String modelLastname) {
-    this.modelLastname = modelLastname;
-  }
+    public void setModelLastname(@NonNull String modelLastname) {
+        this.modelLastname = modelLastname;
+    }
 
-  public String getModelAddress() {
-    return modelAddress;
-  }
+    public String getModelAddress() {
+        return modelAddress;
+    }
 
-  public void setModelAddress(String modelAddress) {
-    this.modelAddress = modelAddress;
-  }
+    public void setModelAddress(String modelAddress) {
+        this.modelAddress = modelAddress;
+    }
 
-  public String getModelPhone() {
-    return modelPhone;
-  }
+    public String getModelPhone() {
+        return modelPhone;
+    }
 
-  public void setModelPhone(String modelPhone) {
-    this.modelPhone = modelPhone;
-  }
+    public void setModelPhone(String modelPhone) {
+        this.modelPhone = modelPhone;
+    }
 
-  public String getModelEmail() {
-    return modelEmail;
-  }
+    public String getModelEmail() {
+        return modelEmail;
+    }
 
-  public void setModelEmail(String modelEmail) {
-    this.modelEmail = modelEmail;
-  }
+    public void setModelEmail(String modelEmail) {
+        this.modelEmail = modelEmail;
+    }
 
-  public List<String> getImages() {
-    return images;
-  }
+    public List<String> getImages() {
+        return images;
+    }
 
-  public void setImages(List<String> images) {
-    this.images = images;
-  }
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
 
-  public Bitmap getModelSignature() {
-    return modelSignature;
-  }
+    public Bitmap getModelSignature() {
+        return modelSignature;
+    }
 
-  public void setModelSignature(Bitmap modelSignature) {
-    this.modelSignature = modelSignature;
-  }
+    public void setModelSignature(Bitmap modelSignature) {
+        this.modelSignature = modelSignature;
+    }
 
-  public boolean isValid() {
-    return this.isRead
-        && this.dateLong != 0
-        && !this.location.isEmpty()
-        && !this.modelFirstname.isEmpty()
-        && !this.modelLastname.isEmpty()
-        && (this.modelAddress != null && !this.modelAddress.isEmpty())
-        && this.modelSignature != null;
-  }
+    public boolean isValid() {
+        return this.isRead
+                && this.dateLong != 0
+                && !this.location.isEmpty()
+                && !this.modelFirstname.isEmpty()
+                && !this.modelLastname.isEmpty()
+                && (this.modelAddress != null && !this.modelAddress.isEmpty())
+                && this.modelSignature != null;
+    }
 
-  public boolean hasMinFields() {
-    return this.dateLong != 0
-        && !this.location.isEmpty()
-        && !this.modelFirstname.isEmpty()
-        && !this.modelLastname.isEmpty();
-  }
+    public boolean hasMinFields() {
+        return this.dateLong != 0
+                && !this.location.isEmpty()
+                && !this.modelFirstname.isEmpty()
+                && !this.modelLastname.isEmpty();
+    }
 }
